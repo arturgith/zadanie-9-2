45a0458a140589a964829b5654e7b3116ad3dde5
Artur Marcinkowski <arturmarcinkowski@interia.pl>
2018-03-24 20:32:38 +0100

    Add files and add new code to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// scripts.js
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);
+
+var newName = 'Marian';
+
+if (allNames.indexOf(newName) === -1) {
+	allNames.push(newName);
+	console.log("Dodano " + newName +" do tablicy " + allNames);
+} else {
+	console.log("Jest już takie imię.")
+}
